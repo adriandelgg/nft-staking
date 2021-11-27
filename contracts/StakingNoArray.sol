e05ae02dca7a8d86761b407739a53e647419b0cc
Adrian Delgado <adriandelgadoc1@gmail.com>
2021-11-26 16:56:00 -0800

    Added both contract

--- contracts/StakingNoArray.sol
+++ contracts/StakingNoArray.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Staking is ERC1155Holder, ReentrancyGuard, Ownable {
+contract StakingNoArray is ERC1155Holder, ReentrancyGuard, Ownable {
 	IERC1155 public nftToken;
 	IERC20 public erc20Token;
 
