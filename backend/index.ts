401e670ed6ceeaddf35925d126c7a9d73696e362
Adrian Delgado <adriandelgadoc1@gmail.com>
2021-12-12 20:21:55 -0800

    Set up backend

--- backend/index.ts
+++ backend/index.ts
@@ -7,6 +7,17 @@ import helmet from "helmet";
 const app = express();
 
 mongoose
-	.connect("")
+	.connect(
+		"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+	)
 	.then(() => console.log("Connected to MongoDB..."))
 	.catch(err => console.error("FAILED to connect to MongoDB: " + err));
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
